6ce7ce5ecebc57ada23be5bae000a68bb42bd088
razilsyah <razilsyah5@gmail.com>
2020-12-07 09:03:38 +0700

    latihan encapsulasi

--- latihan_oop_sederhana/2.latihan_encapsulasi.py
+++ latihan_oop_sederhana/2.latihan_encapsulasi.py
@@ -15,7 +15,7 @@ class Hero:
         self.__attStandar = attack
         self.__armorStandar = armor
         self.__level = 1
-        self.exp = 0
+        self.__exp = 0
 
         self.__healthMax = self.__healthStandar * self.__level
         self.__attMax = self.__attStandar * self.__level
@@ -28,10 +28,39 @@ class Hero:
 
     @property
     def info(self):
-        return f"{self.__name} : \n\thealth : {self.__health}/{self.__healthMax} \n\tattack : {self.__attMax}" \
+        return f"{self.__name} level {self.__level}: \n\thealth : {self.__health}/{self.__healthMax} \n\tattack : {self.__attMax}" \
                f"\n\tarmor : {self.__armorMax}"
 
+    @property
+    def gainExp(self):
+        pass
 
+    @gainExp.setter
+    def gainExp(self,addexp):
+        self.__exp += addexp
+        if self.__exp >= 100:
+            print(self.__name," level up")
+            self.__level += 1
+            self.__exp -= 100
+
+            self.__healthMax = self.__healthStandar * self.__level
+            self.__attMax = self.__attStandar * self.__level
+            self.__armorMax = self.__armorStandar * self.__level
+
+
+    def attack(self,musuh):
+        self.gainExp = 50
 
 rozer = Hero("razor",100,10,5)
+diluc = Hero("diluc",100,5,10)
 print(rozer.info)
+
+
+rozer.attack(diluc)
+rozer.attack(diluc)
+rozer.attack(diluc)
+print(rozer.info)
+
+print(diluc.gainExp)
+
+
