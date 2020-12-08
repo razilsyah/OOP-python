e38f7421df05cad2194ec9294e1846fcfd97225f
razilsyah <razilsyah5@gmail.com>
2020-12-08 12:43:17 +0700

    encapsulasi

--- 5.encapsulasi/encapsulasi.py
+++ 5.encapsulasi/encapsulasi.py
@@ -1,12 +1,13 @@
-# encapsulasi adalah membuat semua properti dan method menjadi private
-# getter mengambil value pada variable private
-# setter
+# encapsulasi buat semua variable private,dan untuk mengambil data yang di butuhkan kita menggunakan getter & setter
+# getter mengambil variable private
+# setter mensetting variable private
 class Hero:
 
-    def __init__(self,nama,health):
+    def __init__(self,nama,health,attpower):
         # variable objek
         self.__nama = nama
         self.__health = health
+        self.__attpower = attpower
 
 
     #getter
@@ -16,25 +17,39 @@ class Hero:
     def getHealth(self):
         return self.__health
 
+    def getAttack(self):
+        return self.__attpower
+
 
     # setter
-    def setNama(self,namaBaru):
-        self.__nama = namaBaru
+    def set_att_power(self,nilaibaru):
+        self.__attpower = nilaibaru
 
-    def setHealth(self,musuh):
+    def diserang(self,musuh):
         self.__health -= musuh
 
+# awal dari game
+razil = Hero("razilsyah",100,5)
+print(razil.__dict__)
 
-razil = Hero("razilsyah",100)
+# mencoba merubah var private ,alhasil malah menambah var baru
+# razil.__nama = "diluc"
+# print(razil.__dict__)
+# print(razil.__nama)
 # print(razil.__nama) ini akan error
 
-#getter
-print(razil.getNama())
-print(razil.getHealth())
+print("==========================================")
 
-#setter
-razil.setNama("wolf")
+#jadi untuk mengambil var private agar tidak error pakai getter
 print(razil.getNama())
+print(f'health : {razil.getHealth()}')
+print('==========================================')
 
-razil.setHealth(75)
-print(razil.getHealth())
+
+#setter mensetting var private
+razil.diserang(90)
+print(f'health : {razil.getHealth()}')
+
+razil.set_att_power(80)
+print(f'attack : {razil.getAttack()}')
+
